78ef4700475d8d7d0f007c123ed65c92f7bac703
Ankush Nakaskar <ankush.nakaskar@phonepe.com>
2026-09-19 13:29:39 +0530

    Added changes related UI changes

--- src/main/java/com/langchain/central/LangchainApp.java
+++ src/main/java/com/langchain/central/LangchainApp.java
@@ -7,12 +7,24 @@ import com.langchain.central.resource.LangChainResource;
 import in.vectorpro.dropwizard.swagger.SwaggerBundle;
 import in.vectorpro.dropwizard.swagger.SwaggerBundleConfiguration;
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
 import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
 import io.dropwizard.configuration.SubstitutingSourceProvider;
 import io.dropwizard.forms.MultiPartBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import java.io.IOException;
+import java.util.EnumSet;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.servlet.FilterHolder;
 import ru.vyarus.dropwizard.guice.GuiceBundle;
 import ru.vyarus.dropwizard.guice.injector.lookup.InjectorLookup;
 
@@ -20,6 +32,8 @@ import ru.vyarus.dropwizard.guice.injector.lookup.InjectorLookup;
  * @author ankush.nakaskar
  */
 public class LangchainApp extends Application<BasicConfiguration> {
+
+    private static final String UI_PATH = "/ui";
 
     protected GuiceBundle guiceBundle;
 
@@ -35,6 +49,28 @@ public class LangchainApp extends Application<BasicConfiguration> {
                 .orElseThrow(() -> new IllegalStateException("Guice injector is not available"));
         environment.jersey().register(injector.getInstance(LangChainResource.class));
 
+        redirectRootToUi(environment);
+    }
+
+
+    private void redirectRootToUi(final Environment environment) {
+        environment.getApplicationContext().addFilter(new FilterHolder(new Filter() {
+            @Override
+            public void doFilter(final ServletRequest request,
+                                 final ServletResponse response,
+                                 final FilterChain chain) throws IOException, ServletException {
+
+                final var httpRequest = (HttpServletRequest) request;
+                final String uri = httpRequest.getRequestURI();
+
+                if ("/".equals(uri) || UI_PATH.equals(uri)) {
+                    ((HttpServletResponse) response)
+                            .sendRedirect(httpRequest.getContextPath() + UI_PATH + "/");
+                    return;
+                }
+                chain.doFilter(request, response);
+            }
+        }), "/*", EnumSet.of(DispatcherType.REQUEST));
     }
 
     @Override
@@ -46,6 +82,7 @@ public class LangchainApp extends Application<BasicConfiguration> {
 
         bootstrap.addBundle(new MultiPartBundle());
         bootstrap.addBundle(swaggerBundle());
+        bootstrap.addBundle(new AssetsBundle("/assets", UI_PATH, "index.html", "ui"));
         bootstrap.setConfigurationSourceProvider(new ResourceConfigurationSourceProvider());
 
 
